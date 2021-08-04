2f7dc82fd6b5dc14a780ec50c24e6b7b6b9d72b2
Fatih Alıcı <fatih.alici44@gmail.com>
2021-08-04 12:32:50 +0300

    Quiz table created.Table filled with faker library.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,16 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::insert([
-            'name'=>'Fatih Alıcı',
-            'email'=>'fatih.alici44@gmail.com',
-            'email_verified_at' => now(),
-            'type'=>'admin',
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+        $this->call([
+            UserSeeder::class,
+            QuizSeeder::class,
         ]);
-
-        \App\Models\User::factory(10)->create();
-
     }
 }
