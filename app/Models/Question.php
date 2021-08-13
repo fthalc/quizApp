d25e68ff3c59f5567d7e281662289aa7fd3e2394
Fatih Alıcı <fatih.alici44@gmail.com>
2021-08-13 22:43:56 +0300

    crate a question and add an image to the question.

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     use HasFactory;
+    //Fillabel hangi alanların girlimesi gerektiğini göstermemiz lazım
+    //yoksa tokende patluyoruz
+    //her seferinde _token göndermemiz de gerekiyor
+    protected $fillable=[
+        'question',
+        'answer1',
+        'answer2',
+        'answer3',
+        'answer4',
+        'answer5',
+        'correct_answer',
+        'image',
+    ];
 }
